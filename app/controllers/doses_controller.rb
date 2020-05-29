7c42fc751455bbac31f7b63367eab906136d1bdf
Arthur Imbert <arthur.imbert@kedgebs.com>
2020-05-29 12:26:25 +0200

    ok

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,7 +3,7 @@ class DosesController < ApplicationController
     @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
-​
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
@@ -15,15 +15,15 @@ class DosesController < ApplicationController
       render "cocktails/show"
     end
   end
-​
+
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail)
   end
-​
+
   private
-​
+
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
